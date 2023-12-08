ba47502d9757737b4f0ad08e9ea5314adbc0546d
freedom <957420317@qq.com>
2023-12-08 08:42:31 +0800

    美化代码格式

--- components/Build.vue
+++ components/Build.vue
@@ -2,19 +2,21 @@
  * @Author: freedom 957420317@qq.com
  * @Date: 2023-12-06 20:41:55
  * @LastEditors: freedom 957420317@qq.com
- * @LastEditTime: 2023-12-07 08:38:04
+ * @LastEditTime: 2023-12-08 08:08:40
  * @FilePath: \blog_before_vue3_nuxt\components\Build.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <script setup>
 import utils from '~/utils/util';
-
+import Prism from 'prismjs'
 import { ref, nextTick } from 'vue';
 const baseUrl = utils.getBaseUrl();
 let colorMode = ref({});
-let conetent = ref();
+let content = ref();
 let id = 1
 const route = useRoute()
+let url = ref("")
+let userName = ref("");
 if (route.query.id) {
   id = route.query.id
 }
@@ -23,12 +25,34 @@ const getTblContent = async (id) => {
   // 防止客户端执行时，直接返回null
   await nextTick()
   let { data: count } = await useFetch(baseUrl + '/base/getTblContent?ID=' + id)
-  conetent.value = count.value.data.retblContent;
+  content.value = count.value.data.retblContent;
+  getUserName(count.value.data.retblContent.authorId);
+}
+const getUserName = async (id) => {
+  // 防止客户端执行时，直接返回null
+  await nextTick()
+  let { data: username } = await useFetch(baseUrl + '/base/GetUserName?id=' + id)
+  userName.value = username.value.data.userName;
 }
 getTblContent(id);
-onMounted(() => {
+onMounted(async () => {
+  await nextTick()
   colorMode.value = useColorMode();
+  console.log(content.value)
+  Prism.highlightAll();
+  url = window.location.href;
+})
+onUpdated(() => {
+  Prism.highlightAll(); //修改内容后重新渲染
 });
+useHead({
+  bodyAttrs: {
+    "data-prismjs-copy": "复制",
+    "data-prismjs-copy-error": "复制出错",
+    "data-prismjs-copy-success": "已复制"
+  }
+})
+
 </script>
 <template>
   <div class=" w-full mx-auto mt-20 mb-20">
@@ -54,34 +78,55 @@ onMounted(() => {
                 </li>
               </ul>
             </div>
-            <div class=" bg-base-100  p-2" style="width:40%">
-              <h1 v-if="conetent" class="text-3xl font-bold text-center">{{ conetent.title }}</h1>
-              <div v-if="conetent" class="overflow-x-auto mt-4 mb-4">
-                <table class="table border-t">
-                  <tbody>
-                    <!-- row 2 -->
-                    <tr>
-                      <td class="text-left">
-                        <Icon name="ep:clock" size="20" color="black" class="mr-2" />{{
-                          utils.dataFliter(conetent.createTime) }}
-                      </td>
-                      <td class="text-center">
-                        <Icon name="ep:view" size="20" color="black" class="mr-2" />{{ conetent.viemNum }}
-                      </td>
-                      <td class="text-right">
-                        <Icon name="ep:user" size="20" color="black" class="mr-2" />一码界
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+            <div class=" bg-base-100  p-6" style="width:40%">
+              <h1 v-if="content" class="text-3xl font-bold text-center">{{ content.title }}</h1>
+              <div v-if="content" class="overflow-x-auto mt-4 mb-4">
+                <hr class="mt-4 mb-4 " />
+                <div class="container mx-auto">
+                  <ul class="flex flex-wrap">
+                    <li class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
+                      <!-- 列1的内容 -->
+                      <div class="">
+                        <p>
+                          <Icon name="ic:baseline-event-available" size="20" color="black" />发布时间:<span class="ml-2">{{
+                            utils.dataFliter(content.createTime) }}</span>
+                        </p>
+                      </div>
+                    </li>
+                    <li class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 ">
+                      <!-- 列2的内容 -->
+                      <div class=" ">
+                        <p>
+                          <Icon name="ic:baseline-preview" size="20" color="black" />阅读次数:<span class="ml-2">{{
+                            content.viemNum }}</span>
+                        </p>
+                      </div>
+                    </li>
+                    <li class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 ">
+                      <!-- 列3的内容 -->
+                      <div class="">
+                        <p>第三列</p>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <hr class="mt-4 mb-4 " />
               </div>
-              <div v-if="conetent" class=" space-y-2" v-html="conetent.content">
+              <div v-if="content" class="editor-content-view" v-html="content.content">
               </div>
-              <div class="bg-base-content/10 mx-1 my-10 h-px"></div>
-              <div class="h-32 card bg-base-200 rounded-box  space-y-2 p-1">
-                <p>文章作者: blinkfox</p>
-                <p>文章链接: https://blinkfox.github.io/2018/11/24/ruan-jian-she-ji/ruan-jian-cheng-xu-she-ji-yuan-ze/</p>
-                <p>版权声明: 本博客所有文章除特別声明外，均采用 CC BY 4.0 许可协议。转载请注明来源 blinkfox !</p>
+              <hr class="mt-4 mb-4" />
+              <div class="h-32 card bg-base-200 rounded-box  space-y-2 p-6">
+                <p>
+                  <Icon name="ep:user" size="20" color="bg-base-200" class="mr-2" />文章作者: {{ userName }}
+                </p>
+                <p>
+                  <Icon name="ic:baseline-link" size="20" color="bg-base-200" class="mr-2" />文章链接:
+                  {{ url }}
+                </p>
+                <p>
+                  <Icon name="ic:baseline-copyright" size="20" color="bg-base-200" class="mr-2" />版权声明: 本博客所有文章除特別声明外，均采用
+                  CC BY 4.0 许可协议。转载请注明来源 {{ userName }} !
+                </p>
               </div>
             </div>
             <div class=" bg-purple-300" style="width:15%">
@@ -180,3 +225,113 @@ onMounted(() => {
     </div>
   </div>
 </template>
+
+<style>
+.editor-content-view {
+  overflow-x: auto;
+}
+
+.editor-content-view p,
+.editor-content-view li {
+  white-space: pre-wrap;
+  /* 保留空格 */
+}
+
+.editor-content-view h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  /* 保留空格 */
+}
+
+.editor-content-view h1 {
+  font-size: 18px;
+  /* 保留空格 */
+}
+
+.editor-content-view h2 {
+  font-size: 17px;
+  /* 保留空格 */
+}
+
+.editor-content-view h3 {
+  font-size: 16px;
+  /* 保留空格 */
+}
+
+.editor-content-view h4 {
+  font-size: 15px;
+  /* 保留空格 */
+}
+
+.editor-content-view h5 {
+  font-size: 14px;
+  /* 保留空格 */
+}
+
+.editor-content-view h6 {
+  font-size: 13px;
+  /* 保留空格 */
+}
+
+
+.editor-content-view blockquote {
+  border-left: 8px solid #d0e5f2;
+  padding: 10px 10px;
+  margin: 10px 0;
+  background-color: #f1f1f1;
+}
+
+.editor-content-view code {
+  padding: 3px;
+  border-radius: 3px;
+}
+
+.editor-content-view pre>code {
+  display: block;
+  padding: 10px;
+}
+
+.editor-content-view table {
+  border-collapse: collapse;
+}
+
+.editor-content-view td,
+.editor-content-view th {
+  border: 1px solid #ccc;
+  min-width: 50px;
+  height: 20px;
+}
+
+.editor-content-view th {
+  background-color: #f1f1f1;
+}
+
+.editor-content-view ul,
+.editor-content-view ol {
+  padding-left: 20px;
+}
+
+.editor-content-view ul>li {
+
+}
+
+.editor-content-view ol>li {
+  
+}
+
+.editor-content-view input[type="checkbox"] {
+  margin-right: 5px;
+}
+
+.code-toolbar>.toolbar>.toolbar-item {
+  margin-right: 10px;
+  color: white;
+}
+</style>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,12 @@
+/*
+ * @Author: freedom 957420317@qq.com
+ * @Date: 2023-12-06 20:41:55
+ * @LastEditors: freedom 957420317@qq.com
+ * @LastEditTime: 2023-12-08 07:23:16
+ * @FilePath: \blog_before_vue3_nuxt\nuxt.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import prismjs from "vite-plugin-prismjs";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -13,7 +22,7 @@ export default defineNuxtConfig({
     fallback: "dark", /// default theme
     dataValue: "theme", // activate data-theme in <html> tag
     classSuffix: "",
-    storageKey: 'nuxt-color-mode',
+    storageKey: "nuxt-color-mode",
   },
   // css: ["~/assets/main.css"],
   i18n: {
@@ -31,10 +40,31 @@ export default defineNuxtConfig({
   },
   nitro: {
     devProxy: {
-      '/api': {
-        target: 'http://localhost:8888',
+      "/api": {
+        target: "http://localhost:8888",
         changeOrigin: true,
       },
-    }
-  }, 
+    },
+  },
+  vite: {
+    plugins: [
+      prismjs({
+        // 添加支持的高亮的语言, 如果需要支持全部的话改成这样:  languages: "all"
+        languages: "all",
+        // 配置prism插件 toolbar是后面两个插件依赖的插件.
+        // show-language: 显示语言名。
+        // copy-to-clipboard: 添加复制代码功能。
+        plugins: [
+          "inline-color",
+          "line-numbers",
+          "toolbar",
+          "show-language",
+          "copy-to-clipboard",
+        ],
+        // 主题名称,支持的主题可以在 node_moduels中安装此库的目录下寻找。
+        theme: "tomorrow",
+        css: true,
+      }),
+    ],
+  },
 });
